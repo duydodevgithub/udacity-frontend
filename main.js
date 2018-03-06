238caaf1c606615dce6ea8ba5505b838feaa1397
DuyDo <duydo.dev@gmail.com>
2018-03-05 19:17:03 -0600

    finish build triangle

--- main.js
+++ main.js
@@ -135,8 +135,33 @@
 // }
 
 
-for(var i = 0; i <= 25; i++) {
-    for(var j = 0; j < 100; j++) {
-        console.log(i + "-" + j);
-    }
-}
+// for(var i = 0; i <= 25; i++) {
+//     for(var j = 0; j < 100; j++) {
+//         console.log(i + "-" + j);
+//     }
+// }
+
+/*
+ * Programming Quiz: Build A Triangle (5-3)
+ */
+
+// creates a line of * for a given length
+// function makeLine(length) {
+//     var line = "";
+//     for (var j = 1; j <= length; j++) {
+//         line += "* ";
+//     }
+//     return line + "\n";
+// }
+
+// // your code goes here.  Make sure you call makeLine() in your own code.
+// function buildTriangle(x) {
+//     var result = "";
+//     for(var i = 1; i <= x; i++) {
+//         result+= makeLine(i);
+//     }
+//     return result;
+// }
+
+// // test your code by uncommenting the following line
+// console.log(buildTriangle(10));
