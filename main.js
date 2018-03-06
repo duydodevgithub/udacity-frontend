1e7c4b9d42509a519bc9552943ef804327b5ce5d
DuyDo <duydo.dev@gmail.com>
2018-03-06 00:42:58 -0600

    array

--- main.js
+++ main.js
@@ -170,10 +170,9 @@ var bills = [50.23, 19.12, 34.01,
     100.11, 12.15, 9.90, 29.11, 12.99,
     10.00, 99.22, 102.20, 100.10, 6.77, 2.22
 ];
-var totals = [];
-bills.map(function(value, index){
-    totals.push(value*1.15);
-    totals[index] = totals[index].toFixed(2);
-    totals[index] = Number(totals[index]);
-});
+
+var totals = bills.map(function(value){
+    value = Number((value*1.15).toFixed(2));
+    return value;
+})
 console.log(totals);
