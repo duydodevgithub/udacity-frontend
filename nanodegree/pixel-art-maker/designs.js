193fa4ec383a4717917d4a14513f355e8e7ead3f
DuyDo <duydo.dev@gmail.com>
2018-05-12 23:30:39 -0500

    sinifh project1 front end nanodegree

---
+++ nanodegree/pixel-art-maker/designs.js
@@ -0,0 +1,11 @@
+// Select color input
+// Select size input
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+    // Your code goes here!
+    let height = document.getElementById("inputHeight");
+    const sizePicker = document.getElementById("sizePicker");
+    sizePicker.addEventListener("click", test)
+  }
+makeGrid();
