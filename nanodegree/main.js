6000a5d542015cbcddaa712c9949ccd8d41812f1
DuyDo <duydo.dev@gmail.com>
2018-05-22 01:26:41 -0500

    ..

--- nanodegree/main.js
+++ nanodegree/main.js
@@ -201,3 +201,10 @@ for(var row = 0; row < numbers.length; row++){
     }
 }
 // console.log(numbers);
+
+let student1 = { name: 'James', age: 26, gender: 'male' };
+let student2 = { name: 'Julia', age: 27, gender: 'female' };
+let student3 = { name: 'Richard', age: 31, gender: 'male' };
+
+const roster = new WeakSet([student1, student2, student3]);
+console.log(roster);
